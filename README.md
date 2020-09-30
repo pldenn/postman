0a3343f2d21cb1d2de32942d00e5f07fecb35a23
Denis Timofeev <y719xy@gmail.com>
2020-09-30 23:25:03 +0300

    test CI fail

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/89752ikqnbs6xyri/branch/master?svg=true)](https://ci.appveyor.com/project/pldenn/postman/branch/master)
+
 ## Задача №3 - Postman Echo
 
 **Важно**: эту задачу нужно выполнять в отдельном репозитории
--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -1,8 +1,31 @@
 import org.junit.Test;
-
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 public class PostmanTest {
+
     @Test
     public void test(){
+// Given - When - Then
+// Предусловия
 
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=utf-8")
+                .body("тест") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .log().all()
+                .when()
+                .post("/post")
+
+// Проверки
+                .then()
+                .log().all()
+
+                .statusCode(200)
+                .header("content-type","application/json; charset=utf-8")
+
+                .body("data.", is("тест fail"))
+                .body("data", equalTo("тест fail"))
+        ;
     }
 }
